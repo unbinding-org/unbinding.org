13a710808f9809cc97061419bdccbcf3a4f3951d
Jonathan Dupré <hello@jonathandupre.com>
2017-08-25 22:11:36 -0400

    create public folder if non-esistent in html build script

--- bin/build-html.js
+++ bin/build-html.js
@@ -1,11 +1,13 @@
 const fs = require('fs')
 const path = require('path')
 const app = require('../src')
+const mkdirp = require('mkdirp')
 const SEP = '<body><script src="bundle.js"></script></body>'
 const routes = ['/']
 const buf = fs.readFileSync(path.join(__dirname, '../src/index.html'))
 const parts = buf.toString().split(SEP)
 
+mkdirp.sync(path.join(__dirname, '../public'))
 routes.forEach(route => {
   const filename = `${route === '/' ? 'index' : route}.html`
   const routeBody = app.toString(route)
